97f375c8849c6182191bd87cd6e64db3c6b04862
tonioreign <tonioreign@gmail.com>
2022-04-30 13:14:01 -0700

    Added scroll function

--- js/app.js
+++ js/app.js
@@ -1,24 +1,47 @@
 'use strict';
 
-// Modal window
+// Selecting Elements
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
-const openModal = function () {
+// Functions
+
+const openModal = e => {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
 
-const closeModal = function () {
+const closeModal = () => {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+// Event Listeners
+
+// Smooth scrolling button
+btnScrollTo.addEventListener('click', e => {
+  const s1cords = section1.getBoundingClientRect(); // get cords
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+
+  // Supports Older Browsers
+
+  // window.scrollTo({
+  //   left: s1cords.left + window.pageXOffset,
+  //   top: s1cords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+});
+
+// Modal Window
+
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -28,3 +51,6 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+//////////////////////////////////////////////////////////////////
+
